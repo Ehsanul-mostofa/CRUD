47bcb44ae41ceb830ac2a26f38a367643f509d0d
Ehsanul Mostofa <emrafi789@gmail.com>
2022-07-11 21:49:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
+var mysqlConnection = mysql.createConnection ({
+   host:'localhost',
+   user:'root',
+   password:'',
+   database: 'employeedb'
+});
+
+mysqlConnection.connect((err)=>{
+    if(!err){
+        console.log("Database connected");
+    }
+    else{
+        console.log("Error" + JSON.stringify(err, undefined, 2));
+    }
+});
+
+app.listen(3000, () => console.log('Express server is runnig at port no : 3000'));
+
+
+//Get all employees
+app.get('/employees', (req, res) => {
+    mysqlConnection.query('SELECT * FROM employee', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Get an employees
+app.get('/employees/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Delete an employees
+app.delete('/employees/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully.');
+        else
+            console.log(err);
+    })
+});
